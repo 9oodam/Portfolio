62ea501b7fe24bf5d2b4e6f5451c42d816fce3cb
9oodam <127164487+9oodam@users.noreply.github.com>
2023-04-04 04:48:34 +0900

    늦잠 자고싶다

--- main.js
+++ main.js
@@ -1,5 +1,41 @@
+// Navigation 누르면 이동할 위치 지정
+let homeY = document.querySelector(".home").getBoundingClientRect().top + window.pageYOffset;
+let aboutY = document.querySelector(".about").getBoundingClientRect().top + window.pageYOffset;
+let skillsY = document.querySelector(".skills").getBoundingClientRect().top + window.pageYOffset;
+let projectY = document.querySelector(".project").getBoundingClientRect().top + window.pageYOffset;
+let contactY = document.querySelector(".contact").getBoundingClientRect().top + window.pageYOffset;
+
+function moveHome() {
+    window.location.reload();
+    window.scrollTo(0, homeY);
+}
+function moveAbout() {
+    window.scrollTo(0, aboutY);
+}
+function moveSkills() {
+    window.scrollTo(0, skillsY);
+}
+function moveProjects() {
+    window.scrollTo(0, projectY);
+}
+function moveContact() {
+    window.scrollTo(0, contactY);
+}
 
 
-function fadeIn() {
+// Contact에서 전화번호, 이메일주소 복사
+let contactIconTel = document.getElementsByClassName("icon01");
+let telNum = document.getElementById("tel");
+let contactIconEmail = document.getElementsByClassName("icon02");
+let emailNum = document.getElementById("email");
 
+function copyTel() {
+    window.navigator.clipboard.writeText(telNum.textContent);
+    console.log(telNum.textContent);
+    alert("전화번호가 복사 되었습니다.");
+}
+function copyEmail() {
+    window.navigator.clipboard.writeText(emailNum.textContent);
+    console.log(emailNum.textContent);
+    alert("이메일 주소가 복사 되었습니다.");
 }
